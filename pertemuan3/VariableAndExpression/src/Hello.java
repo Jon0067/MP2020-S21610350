847e31f99d24c7fc3b944ae36410f8d2a3b0677f
Jon0067 <63180126+Jon0067@users.noreply.github.com>
2020-06-08 15:15:41 +0700

    Pertemuan ke 3

---
+++ pertemuan3/VariableAndExpression/src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        System.out.println("Wellcome to Multiplatform programing");
+
+        int myFirstNumber = (10 + 20) -(2*10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 22;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal);
+    }
+}
