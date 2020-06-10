6900bad929169bea1d0ad46f80c5de25972cc421
Jon0067 <63180126+Jon0067@users.noreply.github.com>
2020-06-10 14:56:01 +0700

    Pertemuan 5

---
+++ pertemuan5/Exercise_02/src/com/multiplatform/Main.java
@@ -0,0 +1,11 @@
+package com.multiplatform;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myIntValue = 4;
+        double Total = (myIntValue)* 0.45359237d ;
+
+        System.out.println("Hasil dari "+myIntValue+" Pound Adalah = " + Total+ " Kilogram");
+    }
+}
