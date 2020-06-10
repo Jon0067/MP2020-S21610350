f3d021c69c2cad00a8d33a3d36a1d480b3e74fc4
Jon0067 <63180126+Jon0067@users.noreply.github.com>
2020-06-10 15:17:23 +0700

    pertemuan 5

---
+++ pertemuan5/FloatAndDouble/src/com/multiplatform/Main.java
@@ -0,0 +1,27 @@
+package com.multiplatform;
+
+import java.io.PrintStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    float myMinFloat = Float.MIN_VALUE;
+        float myMaxFloat = Float.MAX_VALUE;
+        System.out.println("min float"+myMinFloat);
+        System.out.println("max float"+myMaxFloat);
+
+        double myMinDouble = Double.MIN_VALUE;
+        double myMaxDouble = Double.MAX_VALUE;
+        System.out.println("min float"+myMinDouble);
+        System.out.println("max float"+myMaxDouble);
+
+        int IntValue = 5/3;
+        float flot = 5.00f / 3.00f;
+        double Doble = 5.00d/ 3.00d;
+
+        System.out.println(IntValue);
+        System.out.println(flot);
+        System.out.println(Doble);
+
+    }
+}
